f6895b9e9d06b4397f011c86cc6d5a10e5d245b9
JorgeJoseAbad <jorgejoseabad@gmail.com>
2021-03-04 13:25:53 +0100

    Cambios menores

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -25,35 +25,32 @@ class Elevator {
   }
 
   stop() {
-
     clearInterval(this.interval);
     this.interval=null;
-
   }
 
   update(){
-
     if (this.destination !== undefined){
-            if (this.direction === 'up'){
-             this.floorUp();
-            }
-            else {
-              this.floorDown();
-            }
-
-            this.log();
-            _.remove(this.requests, (val) => {
-              return val === this.floor;
-            }); //retira de las peticiones aquellas que ya se han cumplido (estamos
-                 //en el piso requerido)
-            this._passengersLeave(); //salen pasajeros
-            this._passengersEnter(); //entran pasajeros
-
-            if (this.floor === this.destination){
-                this.destination = undefined;
-            }
+        if (this.direction === 'up'){
+         this.floorUp();
+        }
+        else {
+          this.floorDown();
         }
 
+        this.log();
+
+        _.remove(this.requests, (val) => {
+          return val === this.floor;
+        }); //retira de las peticiones aquellas que ya se han cumplido (estamos
+             //en el piso requerido)
+        this._passengersLeave(); //salen pasajeros
+        this._passengersEnter(); //entran pasajeros
+
+        if (this.floor === this.destination){
+            this.destination = undefined;
+        }
+    }
     else {
            this.destination = this.requests.shift();
            if (this.destination !== undefined){
@@ -61,7 +58,7 @@ class Elevator {
              else this.direction = 'down';
            }
     }
-   if (this.requests[0] === undefined && this.waitingList[0] === undefined) this.stop();
+    if (this.requests[0] === undefined && this.waitingList[0] === undefined) this.stop();
   }
 
   //_ señala "privado"
@@ -78,23 +75,19 @@ class Elevator {
          this.passengers = _.concat(this.passengers, waitingHere);
          this.requests = _.concat(this.requests, newFloors); //union impedia nuevas
                                                             //peticiones
-
          _.forEach(waitingHere, (val) => {
            console.log(val.name + " has entered the elevator.");
          });
   }
 
   _passengersLeave() {
-
     let personsLeaving = _.remove(this.passengers, (val) => {
            return this.floor === val.destinationFloor;
          }); //pasajeros que se bajan en este piso
-
-         _.forEach(personsLeaving, (val) => {
-           console.log(val.name + " has left the elevator.");
-         });
+    _.forEach(personsLeaving, (val) => {
+         console.log(val.name + " has left the elevator.");
+       });
   }
-
 
 
   floorUp() {
@@ -110,18 +103,20 @@ class Elevator {
 
   floorDown() {
     this.direction="down";
-    if (this.floor>0){this.floor=this.floor-1;}
+    if (this.floor>0){
+      this.floor=this.floor-1;
+    }
     else {
       this.floor=this.floor;
     }
   }
 
   call(person) {
-  this.waitingList.push(person);
-       if (!_.includes(this.requests, person.originFloor)){
-         this.requests.push(person.originFloor);
-      }
+    this.waitingList.push(person);
+    if (!_.includes(this.requests, person.originFloor)){
+        this.requests.push(person.originFloor);
     }
+  }
 
   log() {
     console.log("Direction: "+this.direction+" | "+"Floor: "+this.floor);
